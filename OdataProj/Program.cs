e210f5d542087f6b68f294a43ddd4d5f8c4dff6b
RickyPhuoc <phuoc.vantran@aventragroup.com>
2025-04-08 11:59:49 +0700

    update startup configuration to access to ODataController

--- OdataProj/Program.cs
+++ OdataProj/Program.cs
@@ -1,19 +1,38 @@
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.ModelBuilder;
 using OdataProj.DAL;
 using OdataProj.DAL.Repository;
 using OdataProj.DAL.Repository.Interface;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-builder.Services.AddControllers().AddOData(opt =>
-    opt.Select().Filter().OrderBy().Expand().Count().SetMaxTop(100));
+// Migrations and Database Setup - can comment out after database is created
+// Temporary build a service provider for running database migrations
+var tempServiceProvider = builder.Services.BuildServiceProvider();
+
+// Create a scope to get the service provider
+using (var scope = tempServiceProvider.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();  // This applies pending migrations or creates the database if it does not exist
+}
+
+var modelBuilder = new ODataConventionModelBuilder();
+modelBuilder.EntityType<Order>();
+modelBuilder.EntitySet<User>("User");
+modelBuilder.EntitySet<Product>("Product");
+
+builder.Services.AddControllers().AddOData(
+    options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+    "odata", modelBuilder.GetEdmModel()));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
